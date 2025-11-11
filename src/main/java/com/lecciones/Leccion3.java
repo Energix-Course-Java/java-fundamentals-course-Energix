1f8200d196e11681f51155c8b28fd2321998c420
krxxg <u202312629@upc.edu.pe>
2025-11-10 19:26:16 -0500

    feat: add lesson.

--- src/main/java/com/lecciones/Leccion3.java
+++ src/main/java/com/lecciones/Leccion3.java
@@ -1,4 +1,45 @@
 package com.lecciones;
+import java.util.Scanner;
 
-public class Leccion3 {
-}
+   public class Leccion3 {
+       public static void vidas(String[] args) {
+           Scanner sc = new Scanner(System.in);
+
+           int vidaHeroe = 100;
+           String[] enemigos = {"Murcielago", "Goblin", "Zombie"};
+
+           System.out.println("=== MAZMORRA ===");
+           System.out.println("Hay " + enemigos.length + " enemigos\n");
+
+           for (int i = 0; i < enemigos.length; i++) {
+               System.out.println("--- Batalla " + (i + 1) + " ---");
+               System.out.println("Enemigo: " + enemigos[i]);
+
+               int vidaEnemigo = 30;
+
+               while (vidaEnemigo > 0 && vidaHeroe > 0) {
+                   System.out.println("\n1. Atacar");
+                   System.out.print("Accion: ");
+                   int accion = sc.nextInt();
+
+                   if (accion == 1) {
+                       vidaEnemigo = vidaEnemigo - 20;
+                       System.out.println("Atacaste! Vida enemigo: " + vidaEnemigo);
+
+                       if (vidaEnemigo > 0) {
+                           vidaHeroe = vidaHeroe - 10;
+                           System.out.println("Te atacaron! Tu vida: " + vidaHeroe);
+                       }
+                   }
+               }
+
+               if (vidaHeroe <= 0) {
+                   System.out.println("\nGAME OVER");
+                   return;
+               }
+               System.out.println("Enemigo derrotado!\n");
+           }
+
+           System.out.println("COMPLETASTE LA MAZMORRA!");
+       }
+   }
--- src/main/java/com/lecciones/Leccion5.java
+++ src/main/java/com/lecciones/Leccion5.java
@@ -1,4 +1,60 @@
 package com.lecciones;
 
-public class Leccion5 {
-}
+    class Heroe {
+        String nombre;
+        int vida;
+        int ataque;
+
+        public Heroe(String n, int v, int a) {
+            nombre = n;
+            vida = v;
+            ataque = a;
+        }
+
+        public void mostrar() {
+            System.out.println("\n=== " + nombre + " ===");
+            System.out.println("Vida: " + "vida");
+            System.out.println("Ataque: " + ataque);
+        }
+
+        public void atacar(Enemigo enemigo) {
+            System.out.println(nombre + " ataca!");
+            enemigo.recibirDanio(ataque);
+        }
+    }
+
+    class Enemigo {
+        String nombre;
+        int vida;
+
+        public Enemigo(String n, int v){
+            nombre = n;
+            vida = v;
+        }
+
+        public void recibirDanio(int danio) {
+            vida = vida - danio;
+            System.out.println(nombre + " recibe " + danio + " de danio");
+            System.out.println("Vida enemigo: " + vida);
+        }
+        public boolean estaVivo() {
+            return vida > 0;
+        }
+    }
+
+
+    public class Leccion5 {
+        public static void main(String[] args) {
+            Heroe jugador = new Heroe("Warrior", 100, 25);
+            Enemigo goblin = new Enemigo("Goblin", 50);
+
+            jugador.mostrar();
+
+            System.out.println("\n=== COMBATE ===");
+            while (goblin.estaVivo()) {
+                jugador.atacar(goblin);
+            }
+            System.out.println("\nVICTORIA!");
+        }
+    }
+
